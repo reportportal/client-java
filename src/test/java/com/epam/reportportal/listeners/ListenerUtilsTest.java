0870dd1e2ee4ae78d3165c01c5120585fa074802
Andrei Varabyeu <andrei_varabyeu@epam.com>
2017-03-17 12:15:24 +0300

    cleanup unused stuff

--- src/test/java/com/epam/reportportal/listeners/ListenerUtilsTest.java
+++ src/test/java/com/epam/reportportal/listeners/ListenerUtilsTest.java
@@ -20,55 +20,18 @@
  */
 package com.epam.reportportal.listeners;
 
+import org.junit.Test;
+
 import static com.epam.ta.reportportal.ws.model.launch.Mode.DEBUG;
 import static com.epam.ta.reportportal.ws.model.launch.Mode.DEFAULT;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import com.github.avarabyeu.restendpoint.http.exception.RestEndpointIOException;
-import org.junit.After;
-import org.junit.Test;
-import org.slf4j.Logger;
-
 
 public class ListenerUtilsTest {
 
-	@Test
-	public void getLaunchMode() {
-		assertEquals(DEFAULT, ListenersUtils.getLaunchMode("notvalid"));
-		assertEquals(DEBUG, ListenersUtils.getLaunchMode("Debug"));
-	}
+    @Test
+    public void getLaunchMode() {
+        assertEquals(DEFAULT, ListenersUtils.getLaunchMode("notvalid"));
+        assertEquals(DEBUG, ListenersUtils.getLaunchMode("Debug"));
+    }
 
-	@Test
-	public void handleException_restEndpointIOExceptionAndNullLogger() {
-		String message = "console message";
-		RestEndpointIOException exception = new RestEndpointIOException(message);
-		Logger nullLogger = null;
-
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-
-		ListenersUtils.handleException(exception, nullLogger, "");
-		// check System.out.println(...) output
-		assertEquals(String.format("%s%n", message), outContent.toString());
-	}
-
-	@Test
-	public void handleException_restEndpointIOExceptionAndNonNullLogger() {
-		String logMessage = "some log message";
-		RestEndpointIOException exception = new RestEndpointIOException("");
-		Logger loggerMock = mock(Logger.class);
-
-		ListenersUtils.handleException(exception, loggerMock, logMessage);
-
-		verify(loggerMock, times(1)).error(logMessage, exception);
-	}
-
-	@After
-	public void cleanUpOutputStream() {
-		System.setOut(null);
-	}
 }
