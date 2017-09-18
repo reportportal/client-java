47b496d6ba8ff97e66ff589ea593babfaae8cc40
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-09-18 12:47:53 +0300

    Documentation for annotations

--- src/main/java/com/epam/reportportal/annotations/ParameterKey.java
+++ src/main/java/com/epam/reportportal/annotations/ParameterKey.java
@@ -37,7 +37,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.PARAMETER })
-public @interface ReportPortalParameter {
+public @interface ParameterKey {
 
 	/**
 	 * Returns parameter key value
