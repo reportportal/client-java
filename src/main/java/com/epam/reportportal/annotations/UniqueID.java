9f366e4dd8b9f68e294e079c7a8f212175b34c16
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-09-19 10:56:43 +0300

    renamed

--- src/main/java/com/epam/reportportal/annotations/UniqueID.java
+++ src/main/java/com/epam/reportportal/annotations/UniqueID.java
@@ -38,7 +38,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.METHOD })
-public @interface TestItemUniqueID {
+public @interface UniqueID {
 
 	/**
 	 * Returns test item unique ID
