6f719bb9f03987e0cffbabd45e02391142fb9537
avarabyeu <andrei.varabyeu@gmail.com>
2017-11-09 12:14:07 +0300

    simplify API for retries

--- src/main/java/com/epam/reportportal/service/LaunchImpl.java
+++ src/main/java/com/epam/reportportal/service/LaunchImpl.java
@@ -145,9 +145,6 @@ public class LaunchImpl extends Launch {
 			@Override
 			public Maybe<String> apply(String id) throws Exception {
 				rq.setLaunchId(id);
-				if (getParameters().isRerun()) {
-					rq.setRetry(true);
-				}
 				return rpClient.startTestItem(rq).doOnSuccess(logCreated("item")).doOnError(LOG_ERROR).map(TO_ID);
 
 			}
@@ -171,10 +168,6 @@ public class LaunchImpl extends Launch {
 					@Override
 					public MaybeSource<String> apply(String parentId) throws Exception {
 						rq.setLaunchId(launchId);
-						if (getParameters().isRerun()) {
-							rq.setRetry(true);
-						}
-
 						return rpClient.startTestItem(parentId, rq).doOnSuccess(logCreated("item")).doOnError(LOG_ERROR).map(TO_ID);
 					}
 				});
