d51ab17adf5290eb0133c0151f773a42be8fa49e
yumfriez <budaevqwerty@gmail.com>
2019-07-01 16:20:22 +0300

    Around advice has been replaced by before/after/after-throwing

--- src/main/java/com/epam/reportportal/aspect/StepAspect.java
+++ src/main/java/com/epam/reportportal/aspect/StepAspect.java
@@ -13,13 +13,8 @@ import com.google.common.base.Function;
 import com.google.common.collect.Queues;
 import io.reactivex.Maybe;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
@@ -32,8 +27,6 @@ import static com.google.common.base.Throwables.getStackTraceAsString;
  */
 @Aspect
 public class StepAspect {
-
-	public static final Logger LOGGER = LoggerFactory.getLogger(StepAspect.class);
 
 	private static InheritableThreadLocal<String> currentLaunchId = new InheritableThreadLocal<String>();
 
@@ -63,89 +56,79 @@ public class StepAspect {
 
 	}
 
-	@Around(value = "anyMethod() && withStepAnnotation(step)", argNames = "joinPoint, step")
-	public Object aroundStepExecution(ProceedingJoinPoint joinPoint, Step step) throws Throwable {
-		Object result = null;
+	@Before(value = "anyMethod() && withStepAnnotation(step)", argNames = "joinPoint,step")
+	public void startNestedStep(JoinPoint joinPoint, Step step) {
 		if (!step.isIgnored()) {
-			startNestedStep(joinPoint, step);
-			try {
-				result = joinPoint.proceed();
-				finishNestedStep();
-			} catch (Throwable throwable) {
-				failedNestedStep(throwable);
-				throw throwable;
+			MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+
+			Maybe<String> parent = stepStack.get().peek();
+			if (parent == null) {
+				parent = parentId.get();
 			}
+
+			StartTestItemRQ startStepRequest = StepRequestUtils.buildStartStepRequest(signature, step, joinPoint);
+
+			Launch launch = launchMap.get().get(currentLaunchId.get());
+			Maybe<String> stepMaybe = launch.startTestItem(parent, startStepRequest);
+			stepStack.get().push(stepMaybe);
 		}
-		return result;
-	}
-
-	private void startNestedStep(JoinPoint joinPoint, Step step) {
-
-		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-
-		Maybe<String> parent = stepStack.get().peek();
-		if (parent == null) {
-			parent = parentId.get();
-		}
-
-		StartTestItemRQ startStepRequest = StepRequestUtils.buildStartStepRequest(signature, step, joinPoint);
-
-		Launch launch = launchMap.get().get(currentLaunchId.get());
-		Maybe<String> stepMaybe = launch.startTestItem(parent, startStepRequest);
-
-		stepStack.get().push(stepMaybe);
 
 	}
 
-	private void finishNestedStep() {
-
-		Maybe<String> stepId = stepStack.get().poll();
-		if (stepId == null) {
-			return;
-		}
-		FinishTestItemRQ finishStepRequest = StepRequestUtils.buildFinishStepRequest(Statuses.PASSED, Calendar.getInstance().getTime());
-		launchMap.get().get(currentLaunchId.get()).finishTestItem(stepId, finishStepRequest);
-
-	}
-
-	public void failedNestedStep(final Throwable throwable) {
-
-		Maybe<String> stepId = stepStack.get().poll();
-		if (stepId == null) {
-			return;
-		}
-
-		ReportPortal.emitLog(new Function<String, SaveLogRQ>() {
-			@Override
-			public SaveLogRQ apply(String itemId) {
-				SaveLogRQ rq = new SaveLogRQ();
-				rq.setTestItemId(itemId);
-				rq.setLevel("ERROR");
-				rq.setLogTime(Calendar.getInstance().getTime());
-				if (throwable != null) {
-					rq.setMessage(getStackTraceAsString(throwable));
-				} else {
-					rq.setMessage("Test has failed without exception");
-				}
-				rq.setLogTime(Calendar.getInstance().getTime());
-
-				return rq;
+	@AfterReturning(value = "anyMethod() && withStepAnnotation(step)", argNames = "step")
+	public void finishNestedStep(Step step) {
+		if (!step.isIgnored()) {
+			Maybe<String> stepId = stepStack.get().poll();
+			if (stepId == null) {
+				return;
 			}
-		});
-
-		FinishTestItemRQ finishStepRequest = StepRequestUtils.buildFinishStepRequest(Statuses.FAILED, Calendar.getInstance().getTime());
-
-		while (stepId != null) {
+			FinishTestItemRQ finishStepRequest = StepRequestUtils.buildFinishStepRequest(Statuses.PASSED, Calendar.getInstance().getTime());
 			launchMap.get().get(currentLaunchId.get()).finishTestItem(stepId, finishStepRequest);
-			stepId = stepStack.get().poll();
 		}
+	}
 
-		FinishTestItemRQ finishParentRequest = buildFinishTestMethodRq(Statuses.FAILED, Calendar.getInstance().getTime());
-		launchMap.get().get(currentLaunchId.get()).finishTestItem(parentId.get(), finishParentRequest);
+	@AfterThrowing(value = "anyMethod() && withStepAnnotation(step)", throwing = "throwable", argNames = "step,throwable")
+	public void failedNestedStep(Step step, final Throwable throwable) {
+
+		if (!step.isIgnored()) {
+
+			Maybe<String> stepId = stepStack.get().poll();
+			if (stepId == null) {
+				return;
+			}
+
+			ReportPortal.emitLog(new Function<String, SaveLogRQ>() {
+				@Override
+				public SaveLogRQ apply(String itemId) {
+					SaveLogRQ rq = new SaveLogRQ();
+					rq.setTestItemId(itemId);
+					rq.setLevel("ERROR");
+					rq.setLogTime(Calendar.getInstance().getTime());
+					if (throwable != null) {
+						rq.setMessage(getStackTraceAsString(throwable));
+					} else {
+						rq.setMessage("Test has failed without exception");
+					}
+					rq.setLogTime(Calendar.getInstance().getTime());
+
+					return rq;
+				}
+			});
+
+			FinishTestItemRQ finishStepRequest = StepRequestUtils.buildFinishStepRequest(Statuses.FAILED, Calendar.getInstance().getTime());
+
+			while (stepId != null) {
+				launchMap.get().get(currentLaunchId.get()).finishTestItem(stepId, finishStepRequest);
+				stepId = stepStack.get().poll();
+			}
+
+			FinishTestItemRQ finishParentRequest = buildFinishParentRequest(Statuses.FAILED, Calendar.getInstance().getTime());
+			launchMap.get().get(currentLaunchId.get()).finishTestItem(parentId.get(), finishParentRequest);
+		}
 
 	}
 
-	private FinishTestItemRQ buildFinishTestMethodRq(String status, Date endTime) {
+	private FinishTestItemRQ buildFinishParentRequest(String status, Date endTime) {
 		FinishTestItemRQ rq = new FinishTestItemRQ();
 		rq.setEndTime(endTime);
 		rq.setStatus(status);
