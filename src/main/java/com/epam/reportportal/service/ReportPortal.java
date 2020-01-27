3c8aa22f2aee06b73c1821edb214d6c2dde45732
yumfriez <budaevqwerty@gmail.com>
2020-01-27 19:58:30 +0300

    Executor `get` method provided

--- src/main/java/com/epam/reportportal/service/ReportPortal.java
+++ src/main/java/com/epam/reportportal/service/ReportPortal.java
@@ -139,6 +139,13 @@ public class ReportPortal {
 	}
 
 	/**
+	 * @return Report Portal {@link ExecutorService}
+	 */
+	public ExecutorService getExecutor() {
+		return executor;
+	}
+
+	/**
 	 * Creates new builder for {@link ReportPortal}
 	 *
 	 * @return builder for {@link ReportPortal}
