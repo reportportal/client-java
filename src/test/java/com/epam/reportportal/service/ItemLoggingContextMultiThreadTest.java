e4155d953658c03ca32002a1a47359d8576689d7
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-02-18 10:51:22 +0300

    Test implementation in progress

--- src/test/java/com/epam/reportportal/service/ItemLoggingContextMultiThreadTest.java
+++ src/test/java/com/epam/reportportal/service/ItemLoggingContextMultiThreadTest.java
@@ -16,14 +16,49 @@
 
 package com.epam.reportportal.service;
 
+import com.epam.reportportal.listeners.ListenerParameters;
+import io.reactivex.Maybe;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.epam.reportportal.test.TestUtils.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ItemLoggingContextMultiThreadTest {
+
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+	@Mock
+	ReportPortalClient rpClient;
+
+	private ExecutorService clientExecutorService = Executors.newSingleThreadExecutor();
+	private ListenerParameters params;
+	private ReportPortal rp;
+
 	@Before
 	public void prepare() {
-
+		params = new ListenerParameters();
+		params.setEnable(Boolean.TRUE);
+		params.setClientJoin(false);
+		simulateStartLaunchResponse(rpClient);
+		simulateStartTestItemResponse(rpClient);
+		simulateLogResponse(rpClient);
+		rp = new ReportPortal(rpClient, clientExecutorService, params, null);
 	}
 
 	@After
@@ -37,7 +72,25 @@ public class ItemLoggingContextMultiThreadTest {
 	 * This test is here to ensure that will never happen again: https://github.com/reportportal/agent-java-testNG/issues/76
 	 */
 	@Test
-	public void test_main_and_other_threads_have_different_logging_contexts() {
+	public void test_main_and_other_threads_have_different_logging_contexts() throws ExecutionException, InterruptedException {
+		// Main thread starts launch and suite
+		Launch launch = rp.newLaunch(standardLaunchRequest(params));
+		Maybe<String> launchId = launch.start();
+		Maybe<String> suiteRs = launch.startTestItem(standardStartSuiteRequest());
+		String suiteId = suiteRs.blockingGet();
 
+		// First thread starts its item and logs data
+		ExecutorService firstThreadExecutor = Executors.newSingleThreadExecutor();
+		Maybe<String> firstItemRs = firstThreadExecutor.submit(() -> launch.startTestItem(suiteRs, standardStartTestRequest())).get();
+		firstThreadExecutor.submit(() -> ReportPortal.emitLog("First thread message", "INFO", new Date())).get();
+
+		// Second thread starts its item and logs data
+		ExecutorService secondThreadExecutor = Executors.newSingleThreadExecutor();
+		Maybe<String> secondItemRs = secondThreadExecutor.submit(() -> launch.startTestItem(suiteRs, standardStartTestRequest())).get();
+		secondThreadExecutor.submit(() -> ReportPortal.emitLog("Second thread message", "INFO", new Date())).get();
+
+		verify(rpClient, times(1)).startLaunch(any());
+		verify(rpClient, times(1)).startTestItem(any());
+		verify(rpClient, times(2)).startTestItem(eq(suiteId), any());
 	}
 }
---
+++ src/test/java/com/epam/reportportal/test/TestUtils.java
@@ -0,0 +1,117 @@
+package com.epam.reportportal.test;
+
+import com.epam.reportportal.listeners.ListenerParameters;
+import com.epam.reportportal.service.ReportPortalClient;
+import com.epam.ta.reportportal.ws.model.EntryCreatedAsyncRS;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import com.epam.ta.reportportal.ws.model.item.ItemCreatedRS;
+import com.epam.ta.reportportal.ws.model.launch.StartLaunchRQ;
+import com.epam.ta.reportportal.ws.model.launch.StartLaunchRS;
+import com.epam.ta.reportportal.ws.model.log.SaveLogRQ;
+import io.reactivex.Maybe;
+import org.mockito.stubbing.Answer;
+
+import java.util.Calendar;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.Optional.ofNullable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class TestUtils {
+
+	private TestUtils() {
+	}
+
+	public static Maybe<StartLaunchRS> startLaunchResponse(String id) {
+		final StartLaunchRS rs = new StartLaunchRS();
+		rs.setId(id);
+		return getConstantMaybe(rs);
+	}
+
+	private static <T> Maybe<T> getConstantMaybe(final T rs) {
+		return Maybe.create(emitter -> {
+			emitter.onSuccess(rs);
+			emitter.onComplete();
+		});
+	}
+
+	public static void simulateStartLaunchResponse(final ReportPortalClient client) {
+		when(client.startLaunch(any(StartLaunchRQ.class))).then((Answer<Maybe<StartLaunchRS>>) invocation -> {
+			StartLaunchRQ rq = invocation.getArgument(0);
+			return startLaunchResponse(ofNullable(rq.getUuid()).orElseGet(() -> UUID.randomUUID().toString()));
+		});
+	}
+
+	public static StartLaunchRQ standardLaunchRequest(final ListenerParameters params) {
+		StartLaunchRQ rq = new StartLaunchRQ();
+		rq.setName(params.getLaunchName());
+		rq.setStartTime(Calendar.getInstance().getTime());
+		rq.setAttributes(params.getAttributes());
+		rq.setMode(params.getLaunchRunningMode());
+		rq.setRerun(params.isRerun());
+		rq.setStartTime(Calendar.getInstance().getTime());
+		return rq;
+	}
+
+	public static Maybe<ItemCreatedRS> startTestItemResponse(String id) {
+		final ItemCreatedRS rs = new ItemCreatedRS();
+		rs.setId(id);
+		return getConstantMaybe(rs);
+	}
+
+	public static void simulateStartTestItemResponse(final ReportPortalClient client) {
+		when(client.startTestItem(any(StartTestItemRQ.class))).then((Answer<Maybe<ItemCreatedRS>>) invocation -> {
+			StartTestItemRQ rq = invocation.getArgument(0);
+			return startTestItemResponse(ofNullable(rq.getUuid()).orElseGet(() -> UUID.randomUUID().toString()));
+		});
+	}
+
+	public static void simulateStartChildTestItemResponse(final ReportPortalClient client) {
+		when(client.startTestItem(anyString(), any(StartTestItemRQ.class))).then((Answer<Maybe<ItemCreatedRS>>) invocation -> {
+			StartTestItemRQ rq = invocation.getArgument(0);
+			return startTestItemResponse(ofNullable(rq.getUuid()).orElseGet(() -> UUID.randomUUID().toString()));
+		});
+	}
+
+	public static StartTestItemRQ standardStartSuiteRequest() {
+		StartTestItemRQ rq = new StartTestItemRQ();
+		rq.setStartTime(Calendar.getInstance().getTime());
+		String id = System.currentTimeMillis() + "-" + Thread.currentThread().getId() + "-" + ThreadLocalRandom.current()
+				.nextInt(9999);
+		rq.setName("Suite_" + id);
+		rq.setDescription("Suite description");
+		rq.setUniqueId(id);
+		rq.setType("SUITE");
+		return rq;
+	}
+
+	public static StartTestItemRQ standardStartTestRequest() {
+		StartTestItemRQ rq = new StartTestItemRQ();
+		rq.setStartTime(Calendar.getInstance().getTime());
+		String id = System.currentTimeMillis() + "-" + Thread.currentThread().getId() + "-" + ThreadLocalRandom.current()
+				.nextInt(9999);
+		rq.setName("Test_" + id);
+		rq.setDescription("Test description");
+		rq.setUniqueId(id);
+		rq.setType("TEST");
+		return rq;
+	}
+
+
+	public static Maybe<EntryCreatedAsyncRS> logResponse(String id) {
+		final EntryCreatedAsyncRS rs = new EntryCreatedAsyncRS();
+		rs.setId(id);
+		return getConstantMaybe(rs);
+	}
+
+	public static void simulateLogResponse(final ReportPortalClient client) {
+		when(client.log(any(SaveLogRQ.class))).then((Answer<Maybe<EntryCreatedAsyncRS>>) invocation -> {
+			StartLaunchRQ rq = invocation.getArgument(0);
+			return logResponse(ofNullable(rq.getUuid()).orElseGet(() -> UUID.randomUUID().toString()));
+		});
+	}
+
+}
