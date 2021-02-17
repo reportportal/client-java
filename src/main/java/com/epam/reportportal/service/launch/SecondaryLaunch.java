5b3f64d7a3fdb0c5e4ba4154affa0e8f053b46a7
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-02-17 19:17:31 +0300

    A bug fixed

--- src/main/java/com/epam/reportportal/service/launch/SecondaryLaunch.java
+++ src/main/java/com/epam/reportportal/service/launch/SecondaryLaunch.java
@@ -93,10 +93,15 @@ public class SecondaryLaunch extends LaunchImpl {
 	@Override
 	public void finish(final FinishExecutionRQ rq) {
 		QUEUE.getUnchecked(launch).addToQueue(LaunchLoggingContext.complete());
+		try {
 			Throwable throwable = Completable.concat(QUEUE.getUnchecked(this.launch).getChildren()).
 					timeout(getParameters().getReportingTimeout(), TimeUnit.SECONDS).blockingGet();
 			if (throwable != null) {
 				LOGGER.error("Unable to finish secondary launch in ReportPortal", throwable);
 			}
+		} finally {
+			// ignore super call, since only primary launch should finish it
+			lockFile.finishInstanceUuid(instanceUuid.get());
+		}
 	}
 }
