1030eac3410cd225e40cd729a74a77cc04d6a39f
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-03-24 17:45:36 +0300

    Fix test

--- src/test/java/com/epam/reportportal/service/LoggingContextTest.java
+++ src/test/java/com/epam/reportportal/service/LoggingContextTest.java
@@ -83,8 +83,9 @@ public class LoggingContextTest {
 	}
 
 	@Test
-	public void test_complete_method_removes_context() {
+	public void test_dispose_method_removes_context() {
 		LoggingContext context = LoggingContext.init(Maybe.just("item_id"));
+		LoggingContext.dispose();
 		assertThat(LoggingContext.context(), anyOf(nullValue(), not(sameInstance(context))));
 	}
 
