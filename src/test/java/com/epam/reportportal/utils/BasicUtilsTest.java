201dc2745075be00ac53bb1ef398683d6f533def
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-09-08 13:19:07 +0300

    Add some tests

---
+++ src/test/java/com/epam/reportportal/utils/BasicUtilsTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BasicUtilsTest {
+	public static Object[][] testData() {
+        //@formatter:off
+        return new Object[][]{
+                // 1. String is longer than limit, truncateReplacement is less than limit
+                {"HelloWorld", 5, "..", "Hel.."},
+                // 2. String is less than limit
+                {"Hello", 10, "...", "Hello"},
+                // 3. String is longer than limit, truncateReplacement is longer than limit
+                {"HelloWorld", 3, "foobar", "Hel"},
+                // 4. Limit is 0
+                {"Hello", 0, "...", ""},
+                // 5. Limit is less than 0
+                {"Hello", -3, "...", ""}
+        };
+        //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void test_truncate_string_scenarios(String input, int limit, String replacement, String expected) {
+        assertThat(BasicUtils.truncateString(input, limit, replacement), equalTo(expected));
+    }
+}
+
+
