c9fa5db7584fddabe26c5ae1aaeb0e65607007ba
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-09-12 13:32:06 +0300

    Fix imports

--- src/main/java/com/epam/reportportal/service/step/StepReporter.java
+++ src/main/java/com/epam/reportportal/service/step/StepReporter.java
@@ -21,12 +21,11 @@ import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
 import com.epam.ta.reportportal.ws.model.OperationCompletionRS;
 import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import io.reactivex.Maybe;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.function.Supplier;
 
 /**
