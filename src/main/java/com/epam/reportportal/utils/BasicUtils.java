566ba912ee55fe249d47c43f1883200b51c82464
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-09-12 13:45:39 +0300

    Replace javax.annotation with jakarta.annotation in BasicUtils

--- src/main/java/com/epam/reportportal/utils/BasicUtils.java
+++ src/main/java/com/epam/reportportal/utils/BasicUtils.java
@@ -16,8 +16,8 @@
 
 package com.epam.reportportal.utils;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
 public class BasicUtils {
 	private BasicUtils() {
