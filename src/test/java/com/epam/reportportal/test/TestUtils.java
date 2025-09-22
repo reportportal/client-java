107a1e35e72d97e783248652dc3d7c5cf84f60c9
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-09-22 13:19:16 +0300

    Fix a warning

--- src/test/java/com/epam/reportportal/test/TestUtils.java
+++ src/test/java/com/epam/reportportal/test/TestUtils.java
@@ -257,7 +257,7 @@ public class TestUtils {
 				.map(b -> {
 					try {
 						return HttpRequestUtils.MAPPER.readValue(
-								b, new TypeReference<List<SaveLogRQ>>() {
+								b, new TypeReference<>() {
 								}
 						);
 					} catch (IOException e) {
