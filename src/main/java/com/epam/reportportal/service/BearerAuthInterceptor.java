7bc5e329f0552522c8f1a17b03539276cac180f1
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-11-03 12:06:18 +0300

    Micro refactoring to speed up BearerAuthInterceptor slightly

--- src/main/java/com/epam/reportportal/service/BearerAuthInterceptor.java
+++ src/main/java/com/epam/reportportal/service/BearerAuthInterceptor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.epam.reportportal.service;
 
 import jakarta.annotation.Nonnull;
@@ -27,16 +28,16 @@ import java.io.IOException;
  */
 public class BearerAuthInterceptor implements Interceptor {
 
-	private final String apiKey;
+	private final String authHeaderValue;
 
 	public BearerAuthInterceptor(String apiKey) {
-		this.apiKey = apiKey;
+		this.authHeaderValue = "Bearer " + apiKey;
 	}
 
 	@Override
 	@Nonnull
 	public Response intercept(Chain chain) throws IOException {
-		Request rq = chain.request().newBuilder().addHeader("Authorization", "Bearer " + apiKey).build();
+		Request rq = chain.request().newBuilder().addHeader("Authorization", authHeaderValue).build();
 		return chain.proceed(rq);
 	}
 }
